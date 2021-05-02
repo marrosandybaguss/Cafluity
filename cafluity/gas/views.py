24eb334576d1c42ccbf6bb212500f4238bd6ddc1
marrosandybaguss <marrosandybaguss@gmail.com>
2021-05-02 15:20:49 +0700

    Initial Form for Z Factor

--- cafluity/gas/views.py
+++ cafluity/gas/views.py
@@ -3,14 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+	if request.method == "POST":
+		gasGravity = request.POST['gas-gravity']
+		pressure = request.POST['pressure']
+		temperature = request.POST['temperature']
+		nitrogen = request.POST['nitrogen']
+		carbonDioxide = request.POST['carbon-dioxide']
+		hydrogenSulfide = request.POST['hydrogen-sulfide']
+	else:
+		gasGravity = 0.7
+		pressure = 1000
+		temperature = 300
+		nitrogen = 3.0
+		carbonDioxide = 6.0
+		hydrogenSulfide = 4.0
+
 	context = {
 		'title':'Compressibility Factor Z',
-		# 'kontributor':'Faqizah',
-		# 'nav':[
-		# 	['/','Home'],
-		# 	['/blog','Blog'],
-		# 	['/about','About'],
-		# 	['/blog/olahraga','Blog Olahraga'],
-		# ],
+		'gasGravity': gasGravity,
+		'pressure': pressure,
+		'temperature': temperature,
+		'nitrogen': nitrogen,
+		'carbonDioxide': carbonDioxide,
+		'hydrogenSulfide': hydrogenSulfide,
 	}
 	return render(request, 'gas/index.html', context)
