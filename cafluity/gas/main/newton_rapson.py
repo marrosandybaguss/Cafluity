4bb9e124715209ab9d6231db3eee3f851f1ee0e0
marrosandybaguss <marrosandybaguss@gmail.com>
2021-05-06 15:43:07 +0700

    max 1k iteration newton raphson

--- cafluity/gas/main/newton_rapson.py
+++ cafluity/gas/main/newton_rapson.py
@@ -7,8 +7,12 @@ def error_aprox(xr_new = 1, xr_old = 0):
 def newton_rapson(e_tol = 0.3, x0 = 0, Tpr = 1, Ppr = 1, func_y = 1, dev_func_y = 1):
   e_aprox = 100
   xr_old = x0
+  iterasi = 1
   while e_aprox > e_tol:
    	xr = Xr(xr_old, Tpr, Ppr, func_y, dev_func_y)
    	e_aprox = error_aprox(xr, xr_old)
    	xr_old = xr
+   	iterasi = iterasi + 1
+   	if iterasi == 1000:
+   		break
   return xr
